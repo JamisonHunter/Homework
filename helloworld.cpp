33c39f5546b95470be223c7569f94bd595bcd979
Jamison Hunter <126278717+JamisonHunter@users.noreply.github.com>
2023-07-12 13:22:04 -0400

    Create helloworld.cpp
    Adding a simple C++ output script

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+// including input & output 
+#include <iostream>
+
+// basic C++ code that outputs a string
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "It's good to be back." << std::endl;
+    return 0;
+}
